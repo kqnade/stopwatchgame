1428ad80e96d23f9d6882350d86368958312c310
K4nade <git@k4na.de>
2023-07-12 07:39:06 +0900

    init

---
+++ script.js
@@ -0,0 +1,53 @@
+// make stopwatch game. 
+// player have to stop the timer at 0.00 but cant watch the timer.
+// If the error with the player's stopped time is within 10 ms, 5000 points are awarded, and for every 10 ms away from that point, 5 stores are deducted.
+// make timer(show millsecoond)
+// and use bulma css framework
+// make start and stop button
+// when playing game, player cant click start button
+// when stop button clicked, timer stop and show score
+// when start button clicked, timer start and score reset
+
+// intialize
+const startbutton = document.getElementById("start");
+const stopbutton = document.getElementById("stop");
+const timer = document.getElementById("timer");
+const score = document.getElementById("score");
+
+let time = 0;
+let timerId = null;
+let scoreValue = 0;
+
+// event listener and run function
+startbutton.addEventListener("click", start);
+stopbutton.addEventListener("click", stop);
+
+//start button clicked, reset score and disable start button. and get current time and start timer
+// and after 1sec, hide timer
+//stop button clicked, stop timer and get current time. start time - stop time = absolute value. and calculate score
+// and show score and enable start button
+// score is calculated by 
+
+function start() {
+    scoreValue = 0;
+    startbutton.disabled = true;
+    time = new Date();
+    timerId = setInterval(() => {
+        timer.innerText = (new Date() - time) / 1000;
+    }, 1);
+    setTimeout(() => {
+        timer.style.display = "none";
+    }, 1000);
+}
+
+function stop() {
+    stoptime = new Date();
+    clearInterval(timerId);
+    timer.style.display = "block";
+    scoreValue = 5000 - (Math.abs((stoptime - time) - 10000) * 5);
+    if ( scoreValue < 0 ) {
+        scoreValue = 0;
+    }
+    score.innerText = scoreValue;
+    startbutton.disabled = false;
+}
